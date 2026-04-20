a4b53f2a202e94e969d3fb60441e04dec84d381d
Nauval Dzakwan Baihaqi <nauvaldzakwanbaihaqi@gmail.com>
2026-04-20 12:22:34 +0700

    Update social media links in index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -171,11 +171,11 @@ export const projects = [
   },
 ];
 export const socials = [
-  { name: "Instagram", href: "https://www.instagram.com/ali.sanatidev/reels/" },
+  { name: "Instagram", href: "https://www.instagram.com/nauvaldzb/" },
   {
     name: "Youtube",
-    href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
+    href: "https://www.youtube.com/@zaccwannn",
   },
-  { name: "LinkedIn", href: "https://www.linkedin.com/in/ali-sanati/" },
-  { name: "GitHub", href: "https://github.com/Ali-Sanati" },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/nauvaldzakwan/" },
+  { name: "GitHub", href: "https://github.com/nauvaldzakwanbaihaqi" },
 ];
